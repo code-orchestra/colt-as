ad2f2f5b5e4756483ec6e21e97adb2e68a81b16f
Eugene Potapenko <potapenko@codeorchestra.com>
2013-08-08 18:23:23 +0200

    html log in pogress

--- src/codeOrchestra/colt/as/ui/TestMainApp.groovy
+++ src/codeOrchestra/colt/as/ui/TestMainApp.groovy
@@ -1,20 +1,9 @@
 package codeOrchestra.colt.as.ui
 
-import com.aquafx_project.AquaFx
-import com.aquafx_project.controls.skin.AquaSkin
-import com.aquafx_project.controls.skin.styles.styler.ScrollBarStyler
-import com.sun.javafx.stage.FocusUngrabEvent
 import javafx.application.Application
-import javafx.application.Platform
-import javafx.event.ActionEvent
-import javafx.event.Event
-import javafx.event.EventHandler
-import javafx.event.EventType
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
-import javafx.scene.input.InputMethodEvent
-import javafx.scene.input.MouseEvent
 import javafx.stage.Stage
 import org.scenicview.ScenicView
 
@@ -29,7 +18,7 @@ class TestMainApp extends Application {
         primaryStage.setTitle("COLT 1.1");
         Scene scene = new Scene(root, 800, 700)
         primaryStage.setScene(scene);
-        //ScenicView.show(scene);
+//       ScenicView.show(scene);
         primaryStage.show();
     }
 
