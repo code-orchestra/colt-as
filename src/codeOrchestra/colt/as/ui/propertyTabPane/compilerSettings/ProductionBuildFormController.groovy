7340049c123dce3a3180de139f4b612eb9d127d5
Dima Kruk <dima.kruk@gmail.com>
2013-08-21 13:20:10 +0200

    ProductionBuildForm

---
+++ src/codeOrchestra/colt/as/ui/propertyTabPane/compilerSettings/ProductionBuildFormController.groovy
@@ -0,0 +1,28 @@
+package codeOrchestra.colt.as.ui.propertyTabPane.compilerSettings
+
+import codeOrchestra.colt.as.model.ModelStorage
+import codeOrchestra.colt.as.model.beans.ProductionBuildModel
+import codeOrchestra.colt.as.ui.components.CTBForm
+import javafx.fxml.FXML
+import javafx.fxml.Initializable
+
+/**
+ * @author Dima Kruk
+ */
+class ProductionBuildFormController implements Initializable {
+
+    @FXML CTBForm compression
+    @FXML CTBForm optimization
+
+    ProductionBuildModel model = ModelStorage.instance.project.projectBuildSettings.productionBuildModel
+
+    @Override
+    void initialize(URL url, ResourceBundle resourceBundle) {
+        bindModel()
+    }
+
+    void bindModel() {
+        compression.checkBox.selectedProperty().bindBidirectional(model.compression())
+        optimization.checkBox.selectedProperty().bindBidirectional(model.optimization())
+    }
+}
