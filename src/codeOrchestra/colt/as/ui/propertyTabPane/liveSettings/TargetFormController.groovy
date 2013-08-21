f3c0c53a023169441e1c21e74deaebab17a1e49e
Dima Kruk <dima.kruk@gmail.com>
2013-08-21 11:31:29 +0200

    TargetForm

--- src/codeOrchestra/colt/as/ui/propertyTabPane/liveSettings/TargetFormController.groovy
+++ src/codeOrchestra/colt/as/ui/propertyTabPane/liveSettings/TargetFormController.groovy
@@ -1,12 +1,13 @@
 package codeOrchestra.colt.as.ui.propertyTabPane.liveSettings
 
-import codeOrchestra.colt.as.model.COLTAsProjectBuildSettings
-import codeOrchestra.colt.as.model.ModelStorage
-import codeOrchestra.colt.as.run.Target
-import codeOrchestra.colt.as.model.beans.RunTargetModel
+import codeOrchestra.colt.as.air.ui.AirFormController
 import codeOrchestra.colt.as.air.ui.android.AndroidAirFormController
 import codeOrchestra.colt.as.air.ui.ios.IOSAirFormController
-import javafx.application.Platform
+import codeOrchestra.colt.as.model.COLTAsProjectBuildSettings
+import codeOrchestra.colt.as.model.ModelStorage
+import codeOrchestra.colt.as.model.beans.RunTargetModel
+import codeOrchestra.colt.as.run.Target
+import codeOrchestra.colt.as.ui.components.RTBForm
 import javafx.beans.property.StringProperty
 import javafx.beans.value.ChangeListener
 import javafx.event.EventHandler
@@ -14,63 +15,31 @@ import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.fxml.Initializable
 import javafx.scene.Scene
-import javafx.scene.control.*
-import javafx.scene.layout.GridPane
-import codeOrchestra.colt.as.air.ui.AirFormController
+import javafx.scene.control.Toggle
+import javafx.scene.control.ToggleGroup
 import javafx.scene.layout.VBox
 import javafx.stage.Modality
 import javafx.stage.Stage
-import javafx.stage.Window
 
 /**
  * @author Dima Kruk
  */
-@SuppressWarnings(["GroovyAssignabilityCheck", "GroovyUnusedDeclaration"])
-public class TargetFormController implements Initializable {
+class TargetFormController implements Initializable {
 
-    @FXML GridPane targetPane
+    ToggleGroup target
+    @FXML RTBForm swf
+    @FXML RTBForm http
+    @FXML RTBForm ios
+    @FXML RTBForm android
 
-    @FXML ToggleGroup target
-    @FXML RadioButton swfRBtn
-    @FXML RadioButton httpRBtn
-    @FXML RadioButton iosRBtn
-    @FXML RadioButton androidRBtn
-
-    @FXML TextField httpTF
-    @FXML TextField iosTF
-    @FXML TextField androidTF
-
-    @FXML Button httpGBtn
-    @FXML Button iosGBtn
-    @FXML Button androidGBtn
-
-    Window window
-
-    private List<Control> controls
     private RunTargetModel model = ModelStorage.instance.project.projectBuildSettings.runTargetModel
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-//        AquaFx.setGroupBox(targetPane)
+    void initialize(URL url, ResourceBundle resourceBundle) {
+        target = new ToggleGroup()
+        target.toggles.addAll(swf.radioButton, http.radioButton, ios.radioButton, android.radioButton)
 
-        controls = [httpTF, iosTF, androidTF, httpGBtn, iosGBtn, androidGBtn]
-
-        controls*.disable = true
-
-        httpRBtn.setUserData([httpTF, httpGBtn])
-        iosRBtn.setUserData([iosTF, iosGBtn])
-        androidRBtn.setUserData([androidTF, androidGBtn])
-
-        target.selectedToggleProperty().addListener({ observableValue, Toggle old_toggle, Toggle new_toggle ->
-            model.target = Target.values()[target.toggles.indexOf(new_toggle)]
-            ((List<Control>) new_toggle?.userData)*.disable = false
-        } as ChangeListener)
-
-        (model.target() as StringProperty).addListener({ prop, oldVal, newVal ->
-            if (newVal) {
-                activateTarget(newVal)
-            }
-        } as ChangeListener)
+        bindModel()
 
         if (!model.target) {
             model.target = "SWF"
@@ -78,7 +47,11 @@ public class TargetFormController implements Initializable {
             activateTarget(model.target)
         }
 
-        iosGBtn.onAction = {
+        http.button.onAction = {
+            //todo: implement
+        } as EventHandler
+
+        ios.button.onAction = {
             if(canShowDialog()) {
                 showDialog(new IOSAirFormController(), "Apple iOS: customize launch", model)
             } else {
@@ -86,39 +59,34 @@ public class TargetFormController implements Initializable {
             }
         } as EventHandler
 
-        androidGBtn.onAction = {
+        android.button.onAction = {
             if(canShowDialog()) {
                 showDialog(new AndroidAirFormController(), "Android: customize launch", model)
             } else {
                 //TODO: show message
             }
         } as EventHandler
-
-        bindModel()
     }
 
     void activateTarget(String newVal) {
-        controls*.disable = true
         Target targetType = Target.valueOf("" + newVal)
-        Toggle selected = target.toggles[targetType.ordinal()]
-        selected.selected = true
+        target.toggles[targetType.ordinal()].selected = true
     }
 
-    boolean canShowDialog() {
+    static boolean canShowDialog() {
         COLTAsProjectBuildSettings buildSettings = ModelStorage.instance.project.getProjectBuildSettings()
         return buildSettings.outputPath && buildSettings.outputFilename
     }
 
     void showDialog(AirFormController controller, String title, RunTargetModel model) {
-        window = androidGBtn.scene.window
         FXMLLoader loader = new FXMLLoader(AirFormController.class.getResource("air_form.fxml"))
         loader.setController(controller)
-        VBox page = loader.load()
+        VBox page = loader.load() as VBox
 
         Stage dialogStage = new Stage()
         dialogStage.title = title
         dialogStage.initModality(Modality.WINDOW_MODAL)
-        dialogStage.initOwner(window)
+        dialogStage.initOwner(swf.scene.window)
         dialogStage.scene = new Scene(page)
 
         controller.setDialogStage(dialogStage)
@@ -130,8 +98,18 @@ public class TargetFormController implements Initializable {
     }
 
     void bindModel() {
-        httpTF.textProperty().bindBidirectional(model.httpIndex())
-        iosTF.textProperty().bindBidirectional(model.iosScript())
-        androidTF.textProperty().bindBidirectional(model.androidScript())
+        target.selectedToggleProperty().addListener({ observableValue, Toggle old_toggle, Toggle new_toggle ->
+            model.target = Target.values()[target.toggles.indexOf(new_toggle)]
+        } as ChangeListener)
+
+        (model.target() as StringProperty).addListener({ prop, oldVal, newVal ->
+            if (newVal) {
+                activateTarget(newVal as String)
+            }
+        } as ChangeListener)
+
+        http.textField.textProperty().bindBidirectional(model.httpIndex())
+        ios.textField.textProperty().bindBidirectional(model.iosScript())
+        android.textField.textProperty().bindBidirectional(model.androidScript())
     }
 }
