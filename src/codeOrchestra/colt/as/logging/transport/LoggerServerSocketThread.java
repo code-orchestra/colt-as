b898bbcc762ca3df2aa126e707b2520e998ff956
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-09-07 17:34:56 +0200

    [CAS-453]

--- src/codeOrchestra/colt/as/logging/transport/LoggerServerSocketThread.java
+++ src/codeOrchestra/colt/as/logging/transport/LoggerServerSocketThread.java
@@ -1,13 +1,14 @@
 package codeOrchestra.colt.as.logging.transport;
 
 import codeOrchestra.colt.core.socket.ServerSocketThread;
+import codeOrchestra.util.SocketUtil;
 
 /**
  * @author Alexander Eliseyev
  */
 public class LoggerServerSocketThread extends ServerSocketThread {
 
-  public static final int LOGGING_PORT = 6126;
+  public static final int LOGGING_PORT = SocketUtil.findAvailablePortStartingFrom(6126);
 
   public LoggerServerSocketThread() {
     super(LOGGING_PORT, LoggerClientSocketHandlerFactory.getInstance());
