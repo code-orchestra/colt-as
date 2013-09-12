b3fe2b204b3d02b58ac79f10fdb8a44fc5972426
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-09-12 13:38:50 +0200

    [CAS-514]

--- src/codeOrchestra/colt/as/model/util/ProjectImporter.groovy
+++ src/codeOrchestra/colt/as/model/util/ProjectImporter.groovy
@@ -18,138 +18,155 @@ class ProjectImporter {
                 name: { s ->
                     project.name = s
                 },
-                outputPath: {String s ->
+                outputPath: { String s ->
                     project.projectBuildSettings.buildModel.outputPath = PathUtils.makeAbsolute(s)
                 },
-                outputFileName: {String s ->
+                outputFileName: { String s ->
                     project.projectBuildSettings.buildModel.outputFileName = s
                 },
-                targetPlayerVersion: {String s ->
+                targetPlayerVersion: { String s ->
                     project.projectBuildSettings.buildModel.targetPlayerVersion = s
                 },
-                mainClass: {String s ->
+                mainClass: { String s ->
                     project.projectBuildSettings.buildModel.mainClass = PathUtils.makeAbsolute(s)
                 },
-                compilationTimeoutValue: {String s ->
+                compilationTimeoutValue: { String s ->
                     project.projectBuildSettings.buildModel.interruptValue = s
                 },
-                compilationTimeout: {String s ->
+                compilationTimeout: { String s ->
                     project.projectBuildSettings.buildModel.interrupt = s
                 },
-                excludeUnusedCode: {String s ->
+                excludeUnusedCode: { String s ->
                     project.projectBuildSettings.buildModel.excludeDeadCode = s
                 },
-                compilerOptions: {String s ->
+                compilerOptions: { String s ->
                     project.projectBuildSettings.buildModel.compilerOptions = s
                 },
-                nonDefaultLocale: {String s ->
+                nonDefaultLocale: { String s ->
                     project.projectBuildSettings.buildModel.nonDefaultLocale = s
                 },
-                localeOptions: {String s ->
+                localeOptions: { String s ->
                     project.projectBuildSettings.buildModel.localeSettings = s
                 },
-                useFrameworkAsRSL: {String s ->
+                useFrameworkAsRSL: { String s ->
                     project.projectBuildSettings.buildModel.rsl = s
                 },
 
-                maxLoopIterations: {String s ->
+                maxLoopIterations: { String s ->
                     project.projectLiveSettings.liveSettingsModel.maxLoop = s as Integer
                 },
-                liveMethods: {String s ->
+                liveMethods: { String s ->
                     project.projectLiveSettings.liveSettingsModel.liveType = s
                 },
-                makeGettersSettersLive: {String s ->
+                makeGettersSettersLive: { String s ->
                     project.projectLiveSettings.liveSettingsModel.makeGSLive = s
                 },
 
-                flexSDKPath: {String s ->
+                flexSDKPath: { String s ->
                     project.projectBuildSettings.sdkModel.flexSDKPath = PathUtils.makeAbsolute(s)
                 },
-                useDefaultSDKConfiguration: {String s ->
+                useDefaultSDKConfiguration: { String s ->
                     project.projectBuildSettings.sdkModel.useFlexConfig = s
                 },
-                useCustomSDKConfiguration: {String s ->
+                useCustomSDKConfiguration: { String s ->
                     project.projectBuildSettings.sdkModel.useCustomConfig = s
                 },
-                customConfigPath: {String s ->
+                customConfigPath: { String s ->
                     project.projectBuildSettings.sdkModel.customConfigPath = PathUtils.makeAbsolute(s)
                 },
 
-                target: {String s ->
+                target: { String s ->
                     project.projectBuildSettings.runTargetModel.target = s
                 },
-                webAddress: {String s ->
+                webAddress: { String s ->
                     project.projectBuildSettings.runTargetModel.httpIndex = s
                 },
-                'air-sdk': {String s ->
+                'air-sdk': { String s ->
                     project.projectBuildSettings.runTargetModel.androidAirModel.airSDKPath = PathUtils.makeAbsolute(s)
                     project.projectBuildSettings.runTargetModel.iosAirModel.airSDKPath = PathUtils.makeAbsolute(s)
                 },
-                airAndroidScript: {String s ->
+                airAndroidScript: { String s ->
                     project.projectBuildSettings.runTargetModel.androidScript = PathUtils.makeAbsolute(s)
                 },
-                airScript: {String s ->
+                airScript: { String s ->
                     project.projectBuildSettings.runTargetModel.iosScript = PathUtils.makeAbsolute(s)
                 },
-                keystore: {String s ->
+                keystore: { String s ->
                     project.projectBuildSettings.runTargetModel.androidAirModel.keystorePath = PathUtils.makeAbsolute(s)
                     project.projectBuildSettings.runTargetModel.iosAirModel.keystorePath = PathUtils.makeAbsolute(s)
                 },
-                storepass: {String s ->
+                storepass: { String s ->
                     project.projectBuildSettings.runTargetModel.androidAirModel.storePass = s
                     project.projectBuildSettings.runTargetModel.iosAirModel.storePass = s
                 },
-                __SAVED_PACKAGED_FILES: {String s ->
+                __SAVED_PACKAGED_FILES: { String s ->
 
                 },
-                'provisioning-profile': {String s ->
+                'provisioning-profile': { String s ->
                     project.projectBuildSettings.runTargetModel.iosAirModel.provisionPath = PathUtils.makeAbsolute(s)
                 },
 
-                sourcePaths: {String s ->
-                    List<File> paths = s.split(/\\:/).collect{ new File(PathUtils.makeAbsolute(it)) }
+                sourcePaths: { String s ->
+                    List<File> paths = splitPaths(s)
                     project.projectPaths.sources = FilesetInput.createFilesetString(paths)
                 },
-                libraryPaths: {String s ->
-                    List<File> paths = s.split(/\\:/).collect{ new File(PathUtils.makeAbsolute(it)) }
+                libraryPaths: { String s ->
+                    List<File> paths = splitPaths(s)
                     project.projectPaths.libraries = FilesetInput.createFilesetString(paths)
                 },
-                asssetPaths: {String s ->
-                    List<File> paths = s.split(/\\:/).collect{ new File(PathUtils.makeAbsolute(it)) }
+                asssetPaths: { String s ->
+                    List<File> paths = splitPaths(s)
                     project.projectPaths.assets = FilesetInput.createFilesetString(paths)
                 },
-                htmlTemplatePath: {String s ->
+                htmlTemplatePath: { String s ->
                     project.projectPaths.htmlTemplatePath = PathUtils.makeAbsolute(s)
                 },
 
-                compression: {String s ->
+                compression: { String s ->
                     project.projectBuildSettings.productionBuildModel.compression = s
                 },
-                optimization: {String s ->
+                optimization: { String s ->
                     project.projectBuildSettings.productionBuildModel.optimization = s
                 },
 
-                clearMessages: {String s ->
+                clearMessages: { String s ->
                     project.projectLiveSettings.settingsModel.clearLog = s
                 },
-                disconnectOnTimeout: {String s ->
+                disconnectOnTimeout: { String s ->
                     project.projectLiveSettings.settingsModel.disconnectOnTimeout = s
                 },
 
-                flashPlayerPath: {String s ->
+                flashPlayerPath: { String s ->
                     project.projectLiveSettings.launcherModel.flashPlayerPath = PathUtils.makeAbsolute(s)
                 },
         ]
 
-        file.eachLine { line ->
-            String[] items = line.split(/=/, 2)
-            String key = items[0]
+        Properties legacyProjectFile = new Properties()
+        legacyProjectFile.load(new FileInputStream(file))
+        legacyProjectFile.propertyNames().each {
+            String key = (String) it
             if (map.containsKey(key)) {
-                map[key](items[1])
+                map[key](legacyProjectFile.getProperty(key))
             }
         }
 
         return project
+    }
+
+    private static List<File> splitPaths(String s) {
+        if (s.contains(":\\")) {
+            return s.split(";").collect { new File(PathUtils.makeAbsolute(it)) }
+        }
+        if (s.contains(":")) {
+            return s.split(":").collect { new File(PathUtils.makeAbsolute(it)) }
+        }
+        if (s.contains(";")) {
+            return s.split(";").collect { new File(PathUtils.makeAbsolute(it)) }
+        }
+
+        List<File> result = new ArrayList<>()
+        result.add(new File(PathUtils.makeAbsolute(s)))
+        return result
     }
 
 }
