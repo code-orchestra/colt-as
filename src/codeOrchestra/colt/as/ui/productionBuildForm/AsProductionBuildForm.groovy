4463efc8d7fa9c12b037f1faa0837c24a7d882ca
Dima Kruk <dima.kruk@gmail.com>
2013-09-17 15:59:13 +0200

    [CAS-535] fixed

--- src/codeOrchestra/colt/as/ui/productionBuildForm/AsProductionBuildForm.groovy
+++ src/codeOrchestra/colt/as/ui/productionBuildForm/AsProductionBuildForm.groovy
@@ -2,40 +2,30 @@ package codeOrchestra.colt.as.ui.productionBuildForm
 
 import codeOrchestra.colt.as.ui.settingsForm.compilerSettings.ProductionBuildForm
 import codeOrchestra.colt.core.tracker.GAController
+import codeOrchestra.colt.core.ui.components.scrollpane.SettingsScrollPane
 import javafx.event.EventHandler
-import javafx.geometry.Pos
 import javafx.scene.control.Button
-import javafx.scene.control.ScrollPane
 import javafx.scene.layout.AnchorPane
-import javafx.scene.layout.VBox
 
 /**
  * @author Dima Kruk
  */
-class AsProductionBuildForm extends ScrollPane{
+class AsProductionBuildForm extends SettingsScrollPane{
     private Button saveAndBuildButton
     EventHandler saveBuildAction
 
     ProductionBuildForm productionBuild
 
     AsProductionBuildForm() {
-        setId("settings-form")
-        styleClass.add("scroll-pane-settings")
-        this.fitToWidth = true
-
-        VBox vBox = new VBox()
-        vBox.alignment = Pos.TOP_CENTER
-        this.content = vBox
-
         productionBuild = new ProductionBuildForm()
         productionBuild.first = true
-        vBox.children.add(productionBuild)
+        mainContainer.children.add(productionBuild)
 
         AnchorPane pane = new AnchorPane()
         saveAndBuildButton = new Button(text: "Save & Build", prefWidth: 100)
         AnchorPane.setRightAnchor(saveAndBuildButton, 10)
         pane.children.add(saveAndBuildButton)
-        vBox.children.add(pane)
+        mainContainer.children.add(pane)
 
         GAController.instance.registerPage(this, "/as/asProductionBuild.html", "asProductionBuild")
     }
