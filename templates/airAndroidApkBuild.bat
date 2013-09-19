efd1f71f1f55950c16772b2684efadf82e6129d4
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-09-19 20:02:45 +0200

    [CAS-536]
    AIR run scripts update

--- templates/airAndroidApkBuild.bat
+++ templates/airAndroidApkBuild.bat
@@ -10,5 +10,5 @@ set OUTPUT_DIR={OUTPUT_DIR}
 cd /d "%OUTPUT_DIR%"
 
 @echo on
-cmd /C "%ADT%" -package -target apk-captive-runtime -storetype pkcs12 -keystore "%CER%" -storepass "%PASS%" {APK_FILE} {DESCRIPTOR_FILE} {PACKAGED_FILES}
-cmd /C "%ADT%" -installApp -platform android -package {APK_FILE}
+cmd /C " "%ADT%" -package -target apk-captive-runtime -storetype pkcs12 -keystore "%CER%" -storepass "%PASS%" {APK_FILE} {DESCRIPTOR_FILE} {PACKAGED_FILES} "
+cmd /C " "%ADT%" -installApp -platform android -package {APK_FILE} "
