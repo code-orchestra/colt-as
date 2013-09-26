3d94bfee51622708e7a3d83fae218a294b3bc67d
Dima Kruk <dima.kruk@gmail.com>
2013-09-26 10:01:51 +0200

    base logic for start test for AS project

--- src/codeOrchestra/colt/as/ui/testmode/AsTestSettingsForm.groovy
+++ src/codeOrchestra/colt/as/ui/testmode/AsTestSettingsForm.groovy
@@ -1,6 +1,8 @@
 package codeOrchestra.colt.as.ui.testmode
 
+import codeOrchestra.colt.as.controller.ColtAsController
 import codeOrchestra.colt.as.model.AsProject
+import codeOrchestra.colt.core.annotation.Service
 import codeOrchestra.colt.core.model.Project
 import codeOrchestra.colt.core.ui.testmode.TestSettingsForm
 import codeOrchestra.util.ProjectHelper
@@ -9,6 +11,7 @@ import codeOrchestra.util.ProjectHelper
  * @author Dima Kruk
  */
 class AsTestSettingsForm extends TestSettingsForm {
+    @Service ColtAsController coltController
     private AsProject project
 
     @Override
@@ -22,5 +25,17 @@ class AsTestSettingsForm extends TestSettingsForm {
         super.init()
         List<String> paths = project.projectPaths.getSourcePaths()
         addDirectories(paths)
+    }
+
+    @Override
+    protected void startTest() {
+        super.startTest()
+        coltController.startBaseCompilation()
+    }
+
+    @Override
+    protected void startRecord() {
+        super.startRecord()
+        coltController.startBaseCompilation()
     }
 }
